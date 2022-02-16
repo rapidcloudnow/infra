5dbb79b950606a92a1144c219a79a42d79c6f28c
vdonkor <vincent.donkor@gmail.com>
2022-02-15 21:11:56 -0500

    added python script to upload files

--- terraform/modules/s3hosting/cloudfront.tf
+++ terraform/modules/s3hosting/cloudfront.tf
@@ -11,6 +11,7 @@ resource "aws_cloudfront_origin_access_identity" "www" {
 resource "aws_cloudfront_origin_access_identity" "redirect" {
   comment = "origin access for rcn redirect"
 }
+
 resource "aws_cloudfront_distribution" "www" {
   enabled = true
   is_ipv6_enabled = true
@@ -64,31 +65,21 @@ resource "aws_cloudfront_distribution" "www" {
 
 # Cloudfront S3 for redirect to www.
 resource "aws_cloudfront_distribution" "redirect" {
-  origin {
-    domain_name = aws_s3_bucket.redirect.bucket_domain_name
-    origin_id = local.redict_orgin_id
-    custom_origin_config {
-      http_port = 80
-      https_port = 443
-      origin_protocol_policy = "http-only"
-      origin_ssl_protocols = ["TLSv1", "TLSv1.1", "TLSv1.2"]
-    }
-  }
   enabled = true
   is_ipv6_enabled = true
+  origin {
+    domain_name = aws_s3_bucket.redirect.bucket_domain_name
+    origin_id = local.www_origin_id
+    s3_origin_config {
+      origin_access_identity = aws_cloudfront_origin_access_identity.redirect.cloudfront_access_identity_path
+    }
+  }
   aliases = [var.domain_name]
   default_cache_behavior {
-    allowed_methods = ["GET", "HEAD"]
-    cached_methods = ["GET", "HEAD"]
+    allowed_methods  = ["GET", "HEAD", "OPTIONS"]
+    cached_methods   = ["GET", "HEAD", "OPTIONS"]
     target_origin_id = local.redict_orgin_id
-
-    forwarded_values {
-      query_string = true
-      cookies {
-        forward = "none"
-      }
-      headers = ["Origin"]
-    }
+    cache_policy_id = data.aws_cloudfront_cache_policy.managed_cache.id
     viewer_protocol_policy = "allow-all"
     min_ttl = 0
     default_ttl = 86400
