d56bb9a446dfabe87cf20714943cce8118acc419
vdonkor <vincent.donkor@gmail.com>
2022-02-15 23:22:12 -0500

    added python script to upload files

--- terraform/modules/s3hosting/route53.tf
+++ terraform/modules/s3hosting/route53.tf
@@ -1,7 +1,7 @@
 #create route53 record for this site
 resource "aws_route53_record" "this" {
   zone_id = data.aws_route53_zone.zone.zone_id
-  name = "www"
+  name = ""
   type = "A"
   alias {
     evaluate_target_health = false
@@ -9,14 +9,12 @@ resource "aws_route53_record" "this" {
     zone_id                = aws_cloudfront_distribution.this.hosted_zone_id
   }
 }
-#create route53 record for root domain
-#resource "aws_route53_record" "redirect" {
-#  zone_id = data.aws_route53_zone.zone.zone_id
-#  name = ""
-#  type = "A"
-#  alias {
-#    evaluate_target_health = false
-#    name                   = aws_cloudfront_distribution.redirect.domain_name
-#    zone_id                = aws_cloudfront_distribution.redirect.hosted_zone_id
-#  }
-#}
+
+resource "aws_route53_record" "www" {
+  count =  var.env ==  "prod" ? 1 : 0
+  name = "www"
+  type = "CNAME"
+  zone_id = data.aws_route53_zone.zone.zone_id
+  ttl = "5"
+  records = [var.domain_name]
+}
